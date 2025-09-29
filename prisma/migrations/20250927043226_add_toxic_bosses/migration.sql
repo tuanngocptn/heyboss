02035392df34bafb58cfbaa789238c433393c6b1
Nick Pham <tuanngocptn@gmail.com>
2025-09-29 12:45:24 +0700

    refactor: update ToxicBoss model and migration for consistent naming conventions
    - Renamed fields in the ToxicBoss model and corresponding migration to use snake_case for improved consistency with database naming conventions.
    - Updated index names in the migration to match the new field names.
    - Ensured that all relevant fields are properly mapped in the Prisma schema for better clarity and maintainability.

--- prisma/migrations/20250927043226_add_toxic_bosses/migration.sql
+++ prisma/migrations/20250927043226_add_toxic_bosses/migration.sql
@@ -1,35 +1,35 @@
 -- CreateTable
 CREATE TABLE "public"."toxic_bosses" (
-    "id" TEXT NOT NULL,
-    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    "updatedAt" TIMESTAMP(3) NOT NULL,
-    "bossName" TEXT NOT NULL,
-    "bossCompany" TEXT,
-    "bossPosition" TEXT,
-    "bossDepartment" TEXT,
-    "bornYear" INTEGER,
-    "workLocation" TEXT,
-    "reporterEmail" TEXT,
-    "reportContent" TEXT NOT NULL,
-    "categories" TEXT[],
-    "markdownPath" TEXT,
-    "pdfPath" TEXT,
-    "locked" BOOLEAN NOT NULL DEFAULT false,
-    "submissionDate" TIMESTAMP(3) NOT NULL,
-    "verified" BOOLEAN NOT NULL DEFAULT false,
-    "published" BOOLEAN NOT NULL DEFAULT false,
+    id TEXT NOT NULL,
+    created_at TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP(3) NOT NULL,
+    boss_name TEXT NOT NULL,
+    boss_company TEXT,
+    boss_position TEXT,
+    boss_department TEXT,
+    born_year INTEGER,
+    work_location TEXT,
+    reporter_email TEXT,
+    report_content TEXT NOT NULL,
+    categories TEXT[],
+    markdown_path TEXT,
+    pdf_path TEXT,
+    locked BOOLEAN NOT NULL DEFAULT false,
+    submission_date TIMESTAMP(3) NOT NULL,
+    verified BOOLEAN NOT NULL DEFAULT false,
+    published BOOLEAN NOT NULL DEFAULT false,
 
     CONSTRAINT "toxic_bosses_pkey" PRIMARY KEY ("id")
 );
 
 -- CreateIndex
-CREATE INDEX "toxic_bosses_bossName_idx" ON "public"."toxic_bosses"("bossName");
+CREATE INDEX "toxic_bosses_boss_name_idx" ON "public"."toxic_bosses"("boss_name");
 
 -- CreateIndex
-CREATE INDEX "toxic_bosses_bossCompany_idx" ON "public"."toxic_bosses"("bossCompany");
+CREATE INDEX "toxic_bosses_boss_company_idx" ON "public"."toxic_bosses"("boss_company");
 
 -- CreateIndex
-CREATE INDEX "toxic_bosses_workLocation_idx" ON "public"."toxic_bosses"("workLocation");
+CREATE INDEX "toxic_bosses_work_location_idx" ON "public"."toxic_bosses"("work_location");
 
 -- CreateIndex
-CREATE INDEX "toxic_bosses_submissionDate_idx" ON "public"."toxic_bosses"("submissionDate");
+CREATE INDEX "toxic_bosses_submission_date_idx" ON "public"."toxic_bosses"("submission_date");
